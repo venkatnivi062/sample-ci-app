203f08563cce612bdfc4df9d9a80c35b779ebd1f
venkatnivi062 <venkatnivi062@gmail.com>
2026-02-02 13:54:54 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,16 @@
-from app import app
+#from app import app
  
-def test_home():
+#def test_home():
+    #client = app.test_client()
+    #response = client.get('/')
+    #assert response.data == b"Hello, CI/CD World!"
+
+from app import create_app
+
+def test_home_page():
+    app = create_app()
     client = app.test_client()
-    response = client.get('/')
-    assert response.data == b"Hello, CI/CD World!"
+
+    response = client.get("/")
+    assert response.status_code == 200
+
